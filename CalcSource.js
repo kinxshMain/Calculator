e043dffc2441f461b93396dbddbe1515905ba14a
kinxshMain <114166303+kinxshMain@users.noreply.github.com>
2022-10-26 12:20:11 -0400

    Create CalcSource.js

---
+++ CalcSource.js
@@ -0,0 +1,1 @@
+javascript: (() => {let op = prompt("What type of operation (MDAS): "); if(op == 'm') {let num1 = prompt('Number 1: '); let num2 = prompt('Number 2: '); window.alert(num1 * num2);} else if(op == 'd') {let num1 = prompt('Number 1: '); let num2 = prompt('Number 2: '); window.alert(num1 / num2);}})();
